49064c0433f6ce6d76d7a49952d2938fea62133e
Pat de Logbaba <webdev.kmer@gmail.com>
2022-09-15 05:19:46 +0100

    add evenlisteners examples

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // Event Listeners
+const btn2 = document.querySelector('.btn-2')
+btn2.addEventListener('click', () => {
+    alert("Alert Box 2")
+})
+
+const btn3 = document.querySelector('.btn-3')
+const box3 = document.querySelector('.box-3')
+
+btn3.addEventListener('click', () => {
+    box3.style.backgroundColor = "blue"
+})
