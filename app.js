896a7d07ed84cf8f0817cf49d36913f88d6c41ee
webdevKmer <webdevKmer@gmail.com>
2022-10-07 18:01:07 +0000

    event handling

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 // Event Listeners
-const btn2 = document.querySelector('.btn-2')
-btn2.addEventListener('click', () => {
-    alert("Alert Box 2")
+const box1 = document.querySelector('.box-1')
+box1.addEventListener('click', () => {
+    alert("Alert Box 1")
 })
 
 const btn3 = document.querySelector('.btn-3')
 const box3 = document.querySelector('.box-3')
 
+const btn2 = document.querySelector('.btn-2')
+const box2 = document.querySelector('.box-2')
+
+btn2.addEventListener('click', () => {
+    box2.classList.toggle('green')
+})
+
 btn3.addEventListener('click', () => {
-    box3.style.backgroundColor = "blue"
+    box3.classList.toggle('blue')
 })
